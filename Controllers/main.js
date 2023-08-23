81fd37afa839c2cf054f6596097affac7e2e2e5b
buinhutminh1999 <buinhutminh1999@gmail.com>
2023-08-23 14:39:56 +0700

    update mẫu in

--- Controllers/main.js
+++ Controllers/main.js
@@ -1,12 +1,11 @@
-
 /*
 Source code được phát triển bởi BuiNhutMinh
 */
-import SanPham from "../Modal/Product.js"
-import Validation from "./Validation.js"
-import DanhSachSanPham from "./DanhSachSanPham.js"
-const validation = new Validation()
-const dssp = new DanhSachSanPham()
+import SanPham from "../Modal/Product.js";
+import Validation from "./Validation.js";
+import DanhSachSanPham from "./DanhSachSanPham.js";
+const validation = new Validation();
+const dssp = new DanhSachSanPham();
 function toggleFullScreen() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
@@ -15,32 +14,36 @@ function toggleFullScreen() {
   }
 }
 
-window.toggleFullScreen = toggleFullScreen
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    toggleFullScreen();
-  }
-}, false);
-
+window.toggleFullScreen = toggleFullScreen;
+document.addEventListener(
+  "keydown",
+  (e) => {
+    if (e.key === "Enter") {
+      toggleFullScreen();
+    }
+  },
+  false
+);
 
 const getMyEle = (select) => {
   return document.querySelector(select);
-}
+};
 
 const getMyEleAll = (select) => {
   return document.querySelectorAll(select);
-}
-
-
+};
 
 const renderSP = (mang) => {
-  if (dssp.mangDS.length == 0) {// nếu dữ liệu trống trả về dữ liệu trống
-    getMyEle('#tblDanhSachSP').innerHTML = `<tr><td colspan="7">Dữ liệu trống</td></tr>`
-    getMyEle('#tfDanhSachSP').innerHTML = ''
+  if (dssp.mangDS.length == 0) {
+    // nếu dữ liệu trống trả về dữ liệu trống
+    getMyEle(
+      "#tblDanhSachSP"
+    ).innerHTML = `<tr><td colspan="7">Dữ liệu trống</td></tr>`;
+    getMyEle("#tfDanhSachSP").innerHTML = "";
   } else {
     let count = 0;
     let newArr = mang.map((item, index) => {
-      let { inpType, inpKTL, inpKLH, inpCH, inpKLV } = item
+      let { inpType, inpKTL, inpKLH, inpCH, inpKLV } = item;
       return `<tr id="test">
     <td>${++count}</td>
     <td>${inpType}</td>
@@ -52,22 +55,21 @@ const renderSP = (mang) => {
     <button onclick="xemSP(${index})" id="xemSPChiTiet"><i class="fa-solid fa-eye"></i></button>
     <button onclick="xoaSP(${index})"><i class="fa-solid fa-trash"></i></button>
     </td>
-    </tr>`
-    })
-    getMyEle('#tfDanhSachSP').innerHTML = `<tr>
+    </tr>`;
+    });
+    getMyEle("#tfDanhSachSP").innerHTML = `<tr>
     <td class="text-right" colspan="7">
     <div class="d-flex align-items-center justify-content-end">
     <button onclick="deleteall()">Xóa tất cả</button>
     <a href="./View/printlabel.html" target="_blank" class="printALL">In tất cả</a>
     </div>
     </td>
-    </tr>`
-    getMyEle('#tblDanhSachSP').innerHTML = newArr.join('')
+    </tr>`;
+    getMyEle("#tblDanhSachSP").innerHTML = newArr.join("");
   }
-}
+};
 
 const printAll = () => {
-
   let newArr = dssp.mangDS.map((item) => {
     return ` <div class="label">
     <br>
@@ -78,136 +80,148 @@ const printAll = () => {
   <div class="content__right">
     <p>${item.inpType}</p>
   <p>TKL: ${item.inpKTL}  KLH: ${item.inpKLH}</p>
-  <p class="klv">KLV: ${item.inpKLV}</p>
+  <span class="klv">KLV: ${item.inpKLV}</span>
   <p>CH: ${item.inpCH}
   </div>
-   </div>`
-  })
-  document.querySelector('.labelProDuct').innerHTML = newArr.join('')
+   </div>`;
+  });
+  document.querySelector(".labelProDuct").innerHTML = newArr.join("");
 
-  getLocal()
+  getLocal();
+};
 
-}
+window.printAll = printAll;
 
-window.printAll = printAll
-
-export default printAll
+export default printAll;
 
 const setLocal = () => {
-  localStorage.setItem('DSSP', JSON.stringify(dssp.mangDS))
-}
+  localStorage.setItem("DSSP", JSON.stringify(dssp.mangDS));
+};
 
 const getLocal = () => {
-  if (localStorage.getItem('DSSP') != null) {
-    dssp.mangDS = JSON.parse(localStorage.getItem('DSSP'))
-    renderSP(dssp.mangDS)
+  if (localStorage.getItem("DSSP") != null) {
+    dssp.mangDS = JSON.parse(localStorage.getItem("DSSP"));
+    renderSP(dssp.mangDS);
   }
-}
+};
 
-getLocal()
+getLocal();
 
 const themSP = () => {
-  let productVal = {}
+  let productVal = {};
 
-  let allProduct = getMyEleAll('.formProduct')
-  console.log(allProduct)
+  let allProduct = getMyEleAll(".formProduct");
+  console.log(allProduct);
 
   for (const element of allProduct) {
-    let { id, value } = element
-    productVal = { ...productVal, [id]: value }
+    let { id, value } = element;
+    productVal = { ...productVal, [id]: value };
   }
-  let { inpType, inpKTL, inpKLH, inpCH, inpKLV, idProDuct } = productVal// giá trị của obj
-  let isValid = true
-  isValid &= validation.kiemtraType()
+  let { inpType, inpKTL, inpKLH, inpCH, inpKLV, idProDuct } = productVal; // giá trị của obj
+  let isValid = true;
+  isValid &= validation.kiemtraType();
   if (isValid) {
-    let sp = new SanPham(inpType, inpKTL.replace(/\s/g, ""), inpKLH.replace(/\s/g, ""), inpCH.replace(/\s/g, ""), inpKLV.replace(/\s/g, ""), idProDuct.replace(/\s/g, ""));
-    dssp.themSP(sp)
-    renderSP(dssp.mangDS)
-    clearData()
-    setLocal()
-    randomInput()
+    let sp = new SanPham(
+      inpType,
+      inpKTL.replace(/\s/g, ""),
+      inpKLH.replace(/\s/g, ""),
+      inpCH.replace(/\s/g, ""),
+      inpKLV.replace(/\s/g, ""),
+      idProDuct.replace(/\s/g, "")
+    );
+    dssp.themSP(sp);
+    renderSP(dssp.mangDS);
+    clearData();
+    setLocal();
+    randomInput();
   }
-}
+};
 
-window.themSP = themSP
+window.themSP = themSP;
 
 const xoaSP = (id) => {
-  dssp.mangDS.splice(id, 1)
-  setLocal()
-  getLocal()
+  dssp.mangDS.splice(id, 1);
+  setLocal();
+  getLocal();
+};
 
-}
-
-window.xoaSP = xoaSP
+window.xoaSP = xoaSP;
 
 const xemSP = (id) => {
-  document.querySelector('.themSP').innerHTML = `
+  document.querySelector(".themSP").innerHTML = `
 
   <button onclick="updateSP(${id})" type="button">Lưu</button>
   
- <button type="button" onclick="huyBoHD()">Hủy bỏ</button>`
-
-
+ <button type="button" onclick="huyBoHD()">Hủy bỏ</button>`;
 
   for (const element in dssp.mangDS[id]) {
-    document.getElementById(element).value = dssp.mangDS[id][element] //obj: dssp.mangDS[id] [element]:gọi đến key của đt ]
+    document.getElementById(element).value = dssp.mangDS[id][element]; //obj: dssp.mangDS[id] [element]:gọi đến key của đt ]
   }
-}
+};
 
-window.xemSP = xemSP
+window.xemSP = xemSP;
 
 const huyBoHD = () => {
-  getMyEle('.themSP').innerHTML = `<button onclick = "themSP()" type="button" id ="btnThem" >Thêm sản phẩm</> `
-}
+  getMyEle(
+    ".themSP"
+  ).innerHTML = `<button onclick = "themSP()" type="button" id ="btnThem" >Thêm sản phẩm</> `;
+};
 
-window.huyBoHD = huyBoHD
+window.huyBoHD = huyBoHD;
 
 let updateSP = (id) => {
-  let objUpdate = {}
-  let productVal = getMyEleAll('.formProduct')
+  let objUpdate = {};
+  let productVal = getMyEleAll(".formProduct");
   for (const element of productVal) {
-    let { id, value } = element
-    objUpdate = { ...objUpdate, [id]: value }
+    let { id, value } = element;
+    objUpdate = { ...objUpdate, [id]: value };
   }
   let { inpType, inpKTL, inpKLH, inpCH, inpKLV, idProDuct } = objUpdate;
-  let spNew = new SanPham(inpType, inpKTL, inpKLH, inpCH, inpKLV, idProDuct)
-  console.log(spNew)
+  let spNew = new SanPham(inpType, inpKTL, inpKLH, inpCH, inpKLV, idProDuct);
 
   // ví dụ code cũ để cập nhật được thì cho dssp.mangds[vitri] = update (update là obj)
-  for (const index in dssp.mangDS[id]) { // muốn hiểu code thế nào thì so sánh cách cũ rồi thay thế code mới 
-    dssp.mangDS[id] = spNew
+  for (const index in dssp.mangDS[id]) {
+    // muốn hiểu code thế nào thì so sánh cách cũ rồi thay thế code mới
+    dssp.mangDS[id] = spNew;
   }
-  document.querySelector('.themSP').innerHTML = `<button onclick = "themSP()" type = "button" id = "btnThem" >Thêm sản phẩm</button>`
-  clearData()
-  randomInput()
-  setLocal()
-  renderSP(dssp.mangDS)
-}
-window.updateSP = updateSP
+  document.querySelector(
+    ".themSP"
+  ).innerHTML = `<button onclick = "themSP()" type = "button" id = "btnThem" >Thêm sản phẩm</button>`;
+  clearData();
+  randomInput();
+  setLocal();
+  renderSP(dssp.mangDS);
+};
+window.updateSP = updateSP;
 const randomInput = () => {
-  document.querySelector('#idProDuct').value = Math.random()
-  document.querySelector('#idProDuct').disabled = true
-}
+  document.querySelector("#idProDuct").value = Math.random();
+  document.querySelector("#idProDuct").disabled = true;
+};
 
-randomInput()
+randomInput();
 
 const clearData = () => {
-  let allProduct = getMyEleAll('.formProduct')
+  let allProduct = getMyEleAll(".formProduct");
   for (const element of allProduct) {
-    document.getElementById(element.id).value = ''
+    document.getElementById(element.id).value = "";
   }
-}
+};
 
 const deleteall = () => {
-  if (confirm('Dữ liệu đã được thêm sẽ được xóa hết. Nhấn Yes để XÓA hoặc CANCEL để hủy hành động này.')) {
+  if (
+    confirm(
+      "Dữ liệu đã được thêm sẽ được xóa hết. Nhấn Yes để XÓA hoặc CANCEL để hủy hành động này."
+    )
+  ) {
     dssp.mangDS = [];
-    getMyEle('#tblDanhSachSP').innerHTML = `<tr> <td colspan="7">Dữ liệu trống</td></tr> `
-    getMyEle('#tfDanhSachSP').innerHTML = ''
+    getMyEle(
+      "#tblDanhSachSP"
+    ).innerHTML = `<tr> <td colspan="7">Dữ liệu trống</td></tr> `;
+    getMyEle("#tfDanhSachSP").innerHTML = "";
     setLocal();
-    return true
+    return true;
   }
-  return false
+  return false;
+};
 
-}
-
-window.deleteall = deleteall
+window.deleteall = deleteall;
