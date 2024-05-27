c0c4670b9beaf991b2becb0845ba7691d342104b
buinhutminh1999 <buinhutminh1999@gmail.com>
2024-05-27 23:49:37 +0700

    update layout, cleancode

--- Modal/Product.js
+++ Modal/Product.js
@@ -1,11 +1,25 @@
-export default class SanPham{
-   constructor(inpType,inpHLV, inpKTL, inpKLH, inpCH,inpKLV,idProDuct){
+export default class SanPham {
+  constructor(
+    inpType,
+    inpHLV,
+    inpKTL,
+    KTLG,
+    inpKLH,
+    KLHG,
+    inpCH,
+    inpKLV,
+    KLVG,
+    idProDuct
+  ) {
     this.inpType = inpType;
-    this.inpHLV = inpHLV
+    this.inpHLV = inpHLV;
     this.inpKTL = inpKTL;
+    this.KTLG = KTLG;
     this.inpKLH = inpKLH;
+    this.KLHG = KLHG;
     this.inpCH = inpCH;
     this.inpKLV = inpKLV;
+    this.KLVG = KLVG;
     this.idProDuct = idProDuct;
-   }
+  }
 }
